27766900884f8765d39fdf6b89629df9470dcd33
metan_ <borealordsss123@gmail.com>
2025-09-22 23:06:32 +0300

    Add Project Rock Paper Scissors

---
+++ Project Rock Paper Scissors/javascript.js
@@ -0,0 +1,36 @@
+let name = "BORIS";
+let surname = "CUZMENCO";
+
+console.log(name);
+console.log(surname);
+
+let age = 11;
+
+console.log(age);
+
+age = 54;
+
+console.log(age);
+
+const pi = 3.14;
+//pi = 10;
+
+console.log(pi);
+
+console.log((3 + 2) - 76 * (1 + 1));
+
+console.log(23 + 97)
+console.log((4 + 6 + 9) / 77)
+
+//4
+let a = 10
+console.log(a)
+a = 20
+console.log(a)
+let b = 7 * a
+console.log(b)
+//5
+const max = 57
+const actual = max - 13
+const percentage = actual / max
+console.log(percentage)
